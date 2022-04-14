eb3101c7cf85d2c9de050781edd39e711843421b
CriSFV <cristina.f.valverde@gmail.com>
2022-04-14 10:08:39 +0200

    add styles and some variable

--- components/Navigation/AppHeader.vue
+++ components/Navigation/AppHeader.vue
@@ -1,26 +1,16 @@
 <template>
-    <header class="header">
-        <MenuLinks/>
-    </header>
+  <header class="header">
+    <MenuLinks />
+  </header>
 </template>
 <script>
-import MenuLinks from './MenuLinks.vue'
 export default {
-    name:'Appheader',
-    components:{
-        MenuLinks
-    },
+  name: 'Appheader',
+  components: {
+    MenuLinks: () => import('./MenuLinks.vue'),
+  },
 }
 </script>
 <style >
-.header {
-    font-weight: 600;
-    position: fixed;
-    height: 60px;
-    top: 0;
-    width: 100%;
-    background-image: linear-gradient(#273bec, rgb(58, 58, 52));
-    z-index: 100;
-    font-size: 5px;
-    }
+@import '@/assets/styles/headerStyle.scss';
 </style>
